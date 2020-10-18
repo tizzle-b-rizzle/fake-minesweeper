ab12bc6eda0e40718194938f11f5d0bf096bff0c
Tyler <tyler.rooker@dxc.com>
2020-10-18 20:35:56 +0100

    Started the js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,26 @@
-function test() {
-    var test = document.getElementById("minesweeper-front-tiles")
-    test.style.display = "none";
+var startOfGame = false;
+var arrayOfBombsOrNot = 
+[
+    B, B, B, B, B, B, B, B, B, B,
+    B, B, NB, NB, NB, NB, NB, NB, NB, NB,
+    NB, NB, NB, NB, NB, NB, NB, NB, NB, NB,
+    NB, NB, NB, NB, NB, NB, NB, NB, NB, NB,
+    NB, NB, NB, NB, NB, NB, NB, NB, NB, NB,
+    NB, NB, NB, NB, NB, NB, NB, NB, NB, NB,
+    NB, NB, NB, NB, NB, NB, NB, NB, NB, NB,
+    NB, NB, NB, NB, NB, NB, NB, NB, NB, NB,
+    NB, NB, NB, NB, NB, NB, NB, NB, NB, NB,
+    NB, NB, NB, NB, NB, NB, NB, NB, NB
+]
+
+function test(a) {
+    if (startOfGame != true) {
+        startofGame = true; //need to add here the function that generates the mines
+    } 
+    switch (a) { //probably make the switch its own function so I can just call it
+        case 1:
+            image = document.getElementById("front-tile-row-1-column-1-img");
+            image.style.display = "none";
+            break;
+    }
 }
