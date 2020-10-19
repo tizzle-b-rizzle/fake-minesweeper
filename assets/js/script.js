c35b9b27497ba5006993930367254d88dd9ae40f
Tyler <tyler.rooker@dxc.com>
2020-10-19 19:28:17 +0100

    Added a bunch of notes to the JS and got further in completing it

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,19 @@
+//to do the bomb check, give a second array a random variable from the the first array so the second array is the order, then use
+//secondArray[a-1] to determine if it's a bomb or not
+/*
+Steps needed:
+1. Once user clicks, the code check to see if it's the first tile clicked
+2. If yes, the code makes sure THAT tile isn't a bomb, and sets the rest to randomly be bombs or not using the arrayOfBombsOrNot and
+random number. The non-bombs need a function to determine the number of the tile
+everytime a value is assigned to a tile, that value is removed from the arrayOfBombsOrNot
+3. If not, the game checks to see if the tile is a bomb or not. If yes, game over, if not then the tile just goes
+*/
 var startOfGame = false;
 var arrayOfBombsOrNot = 
 [
     B, B, B, B, B, B, B, B, B, B,
-    B, B, NB, NB, NB, NB, NB, NB, NB, NB,
-    NB, NB, NB, NB, NB, NB, NB, NB, NB, NB,
+    B, B, NB, NB, NB, NB, NB, NB, NB, NB, 
+    NB, NB, NB, NB, NB, NB, NB, NB, NB, NB, 
     NB, NB, NB, NB, NB, NB, NB, NB, NB, NB,
     NB, NB, NB, NB, NB, NB, NB, NB, NB, NB,
     NB, NB, NB, NB, NB, NB, NB, NB, NB, NB,
@@ -12,11 +22,25 @@ var arrayOfBombsOrNot =
     NB, NB, NB, NB, NB, NB, NB, NB, NB, NB,
     NB, NB, NB, NB, NB, NB, NB, NB, NB
 ]
+var arrayOfGameBombState =
+[
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+    0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+]
+var numberOfTilesClicked = 0; //increase this as the tiles increase, if it equals one do the function
 
-function test(a) {
-    if (startOfGame != true) {
-        startofGame = true; //need to add here the function that generates the mines
-    } 
+function clickTile(a) {
+    if (numberOfTilesClicked == 0) (
+        numberofTilesClicked += 1
+    )
     switch (a) { //probably make the switch its own function so I can just call it
         case 1:
             image = document.getElementById("front-tile-row-1-column-1-img");
@@ -24,3 +48,4 @@ function test(a) {
             break;
     }
 }
+
